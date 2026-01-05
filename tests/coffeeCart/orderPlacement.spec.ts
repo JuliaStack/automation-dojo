788838d3dd214c433dc1117247fa8e59bbcf1ed4
Yuliia Vasylieva <basiliskzp@gmail.com>
2026-01-05 21:34:00 +0200

    Update Playwright config and tests

---
+++ tests/coffeeCart/orderPlacement.spec.ts
@@ -0,0 +1,42 @@
+import { test, expect } from '@playwright/test';
+
+test('TC-7 Place order without promotion', {tag: "@regression"}, async ({ page }) => {
+  await page.goto('https://coffee-cart.app/');
+  await page.getByRole('link', { name: 'Menu page' }).click();
+  await expect(page.locator('[data-test="checkout"]')).toContainText('Total: $0.00');
+  await expect(page.locator('#app')).toContainText('Flat White $18.00');
+  await page.locator('[data-test="Flat_White"]').click();
+  await expect(page.locator('[data-test="checkout"]')).toContainText('Total: $18.00');
+  await page.getByRole('link', { name: 'Cart page' }).click();
+  await expect(page.locator('#app')).toContainText('Flat White$18.00 x 1+-$18.00x');
+  await page.locator('[data-test="checkout"]').click();
+  await expect(page.getByText('Payment details×We will send')).toBeVisible();
+  await page.getByRole('textbox', { name: 'Name' }).fill('test01');
+  await page.getByRole('textbox', { name: 'Email' }).fill('testing@tst.com');
+  await expect(page.getByRole('checkbox', { name: 'Promotion checkbox' })).not.toBeChecked();
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await expect(page.locator('#app')).toContainText('Thanks for your purchase. Please check your email for payment.');
+  await expect(page.locator('[data-test="checkout"]')).toContainText('Total: $0.00');
+});
+
+
+test('TC-8 Place order with promotion', {tag: "@regression"}, async ({ page }) => {
+  await page.goto('https://coffee-cart.app/');
+  await page.locator('[data-test="Espresso"]').click();
+  await page.locator('[data-test="Espresso_Macchiato"]').click();
+  await page.locator('[data-test="Cappuccino"]').click();
+  await expect(page.locator('#app')).toContainText('It\'s your lucky day! Get an extra cup of Mocha for $4.');
+  await page.getByRole('button', { name: 'Yes, of course!' }).click();
+  await page.getByText('cart (4)').click();
+  await expect(page.getByRole('link', { name: 'Cart page' })).toBeVisible();
+  await expect(page.locator('div').filter({ hasText: /^\(Discounted\) Mocha$/ })).toBeVisible();
+  await page.locator('[data-test="checkout"]').click();
+  await expect(page.getByRole('heading', { name: 'Payment details' })).toBeVisible();
+  await page.getByRole('textbox', { name: 'Name' }).click();
+  await page.getByRole('textbox', { name: 'Name' }).fill('order with promo');
+  await page.getByRole('textbox', { name: 'Email' }).click();
+  await page.getByRole('textbox', { name: 'Email' }).fill('testing@tst.com');
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await expect(page.locator('#app')).toContainText('Thanks for your purchase. Please check your email for payment.');
+  await expect(page.locator('[data-test="checkout"]')).toContainText('Total: $0.00');
+});
